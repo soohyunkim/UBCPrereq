d045f37480dcf4f174079db1f21d8f94a1cd4452
Soohyun Kim <shkim7534@gmail.com>
2017-08-30 17:37:10 -0700

    pre-req and co-req filtering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var fs = require('fs');
+// var fs = require('fs');
 
-request("https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=3&dept=CPSC&course=221", function(error, response, body) {
+var url = "https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=3&dept=CPSC&course=221";
+
+request(url, (error, response, body) => {
   if(error) {
     console.log("Error: " + error);
   }
@@ -10,9 +12,11 @@ request("https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&
 
   var $ = cheerio.load(body);
   $('div.container > div.content.expand').each(function( index ) {
-    var prereqFinder = $(this).find("p:contains('Pre-reqs')").text().trim();
-    var course = prereqFinder.match(/\D\D\D\D\s\d\d\d/g);
-    console.log("CourseName: " + course);
+    var coreqCourse = $(this).find("p:contains('Co-reqs')").text().trim().replace(/\s\s+/g, ' ');
+    var prereqCourse = $(this).find("p:contains('Pre-reqs')").text().trim().replace(/\s\s+/g, ' ');
+    prereqCourse = (!prereqCourse) ? 'Pre-reqs: None' : prereqCourse;
+    coreqCourse = (!coreqCourse) ? 'Co-reqs: None' : coreqCourse;
+    console.log(prereqCourse, coreqCourse);
   });
 
 });
