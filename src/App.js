168a094e424e72c703ed00edfa4298956cc3c19f
Soohyun Kim <shkim7534@gmail.com>
2017-08-30 17:02:07 -0700

    scraped CPSC221 page only

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
-import Prereqs from './Prereqs.js';
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
 
-class App extends Component {
-  render() {
-    return (
-      <Prereqs />
-    );
+request("https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=3&dept=CPSC&course=221", function(error, response, body) {
+  if(error) {
+    console.log("Error: " + error);
   }
-}
+  console.log("Status code: " + response.statusCode);
 
-export default App;
+  var $ = cheerio.load(body);
+  $('div.container > div.content.expand').each(function( index ) {
+    var prereqFinder = $(this).find("p:contains('Pre-reqs')").text().trim();
+    var course = prereqFinder.match(/\D\D\D\D\s\d\d\d/g);
+    console.log("CourseName: " + course);
+  });
+
+});
