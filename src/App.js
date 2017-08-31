5132178d06746980fa3fc0eb9efea7e0497d3b09
Soohyun Kim <shkim7534@gmail.com>
2017-08-31 09:06:55 -0700

    set up server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 var request = require('request');
 var cheerio = require('cheerio');
-// var fs = require('fs');
+var express = require('express');
+var app = express();
 
-var url = "https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=3&dept=CPSC&course=221";
-
-request(url, (error, response, body) => {
-  if(error) {
-    console.log("Error: " + error);
-  }
-  console.log("Status code: " + response.statusCode);
-
-  var $ = cheerio.load(body);
-  $('div.container > div.content.expand').each(function( index ) {
-    var coreqCourse = $(this).find("p:contains('Co-reqs')").text().trim().replace(/\s\s+/g, ' ');
-    var prereqCourse = $(this).find("p:contains('Pre-reqs')").text().trim().replace(/\s\s+/g, ' ');
-    prereqCourse = (!prereqCourse) ? 'Pre-reqs: None' : prereqCourse;
-    coreqCourse = (!coreqCourse) ? 'Co-reqs: None' : coreqCourse;
-    console.log(prereqCourse, coreqCourse);
+app.get('/scrape', (req, res) => {
+  
+  url = "https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=3&dept=CPSC&course=221";
+  
+  request(url, (error, response, body) => {
+    (error) ? console.log("Error: " + error) : console.log("Status code: " + response.statusCode);
+  
+    var $ = cheerio.load(body);
+    $('div.container > div.content.expand').each(function( index ) {
+      var coreqCourse = $(this).find("p:contains('Co-reqs')").text().trim().replace(/\s\s+/g, ' ');
+      var prereqCourse = $(this).find("p:contains('Pre-reqs')").text().trim().replace(/\s\s+/g, ' ');
+      prereqCourse = (!prereqCourse) ? 'Pre-reqs: None' : prereqCourse;
+      coreqCourse = (!coreqCourse) ? 'Co-reqs: None' : coreqCourse;
+    res.send(prereqCourse)
+    });
   });
-
 });
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
